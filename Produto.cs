c2b85630508a63b9cccab81183c3734f831621f4
Matheus Alexandre <matheuscurses@gmail.com>
2022-10-30 21:58:06 -0300

    Exemplo Construtores

---
+++ Produto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace Construtores
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+
+        }
+
+        // operação Valor Total em Estoque
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        // essa operação nao retorna um valor de saída, ao contrário, ela adiciona a variavel uma quantidade
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+
+        // Sobreposição
+        public override string ToString()
+        {
+            // Transformar um produto em string
+            // Quando o objeto é colocado dentro de um Console.WriteLine, ele vai chamar o método TToString e faz a conversão
+            return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                    + ", "
+                    + Quantidade
+                    + " unidades, Total: $ "
+                    + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace Construtores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Primeiro é declarado as variáveis que vão receber o que o usuário digitar
+            // Depois ela será jogada na classe onde o atributo vai recebê-las
+
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            // Depois de declarar as variáveis, faça a instanciação do construtor
+
+            Produto p = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+
+            // ler a quantidade do usuário
+            int qte = int.Parse(Console.ReadLine());
+            // acrescentar essa quantidade no produto p
+            // chamar a operação AdicionarProdutos()
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine("Digite a quantidade a ser removidos do estoque: ");
+
+            // ler a quantidade do usuário
+            qte = int.Parse(Console.ReadLine());
+            // acrescentar essa quantidade no produto p
+            // chamar a operação AdicionarProdutos()
+            p.RemoverProdutos(qte);
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
